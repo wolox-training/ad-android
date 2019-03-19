f6f253bc1d7fc44ec47213bef65b410ec026b82e
Alan Donoso Naumczuk <alan.donoso@wolox.com.ar>
2019-03-19 10:35:24 -0300

    Center termsAndConditions textView and renaming id of every layout component

--- app/src/main/java/ar/com/wolox/android/example/ui/example/login/LoginPresenter.java
+++ app/src/main/java/ar/com/wolox/android/example/ui/example/login/LoginPresenter.java
@@ -1,9 +1,5 @@
 package ar.com.wolox.android.example.ui.example.login;
 
-//import javax.inject.Inject;
-//
-//import ar.com.wolox.android.example.utils.UserSession;
-
 import javax.inject.Inject;
 
 import ar.com.wolox.wolmo.core.presenter.BasePresenter;
@@ -11,10 +7,10 @@ import ar.com.wolox.wolmo.core.presenter.BasePresenter;
 /**
  *
  */
-public class LoginPresenter extends BasePresenter<ILoginView> {
+class LoginPresenter extends BasePresenter<ILoginView> {
 
     @Inject
-    public LoginPresenter() {
+    LoginPresenter() {
 
     }
 //    @Inject
