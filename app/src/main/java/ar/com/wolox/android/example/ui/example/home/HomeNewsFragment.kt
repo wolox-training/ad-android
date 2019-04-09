67cd41252aeb7227d67af9b9b24cdefbf79f8c33
Alan Donoso Naumczuk <alan.donoso@wolox.com.ar>
2019-04-09 10:12:16 -0300

    Frontend ready to do the pull request

--- app/src/main/java/ar/com/wolox/android/example/ui/example/home/HomeNewsFragment.kt
+++ app/src/main/java/ar/com/wolox/android/example/ui/example/home/HomeNewsFragment.kt
@@ -1,8 +1,11 @@
 package ar.com.wolox.android.example.ui.example.home
 
+import android.content.Intent
+import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import ar.com.wolox.android.R
 import ar.com.wolox.android.example.model.News
+import ar.com.wolox.android.example.ui.example.signup.SignUpActivity
 import ar.com.wolox.wolmo.core.fragment.WolmoFragment
 import ar.com.wolox.wolmo.core.presenter.BasePresenter
 import kotlinx.android.synthetic.main.fragment_home_news.*
@@ -13,10 +16,24 @@ class HomeNewsFragment @Inject constructor() : WolmoFragment<BasePresenter<Any>>
     override fun init() {
         vHomeNewsRecyclerView.apply {
             // TODO FOR NEXT PR (Backend): change this list for data from a request to DB/API
-            adapter = HomeNewsViewAdapter(listOf(News("Hello",
-                    "Description of the hello"), News("Bye",
-                    "Description of the bye")))
+            adapter = HomeNewsViewAdapter(listOf(News("Detuvieron a Alberto Samid en Belice",
+                    "En Belice, fue detenido por Interpol Alberto Samid. Nuestra" +
+                            " política es clara y transparente. Quienes tienen deudas con la" +
+                            " justicia, deben"), News("Susto para Lionel Scaloni en España:" +
+                    " fue atropellado por un auto y sufrió lesiones", "El DT de la " +
+                    "Argentina iba en bicicleta y fue embestido en el estacionamiento del " +
+                    "colegio de sus hijas. Fue atendido en un hospital y ya recibió el alta.")))
             layoutManager = LinearLayoutManager(context)
+            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+        }
+    }
+
+    override fun setListeners() {
+        vHomeNewsAddButton.setOnClickListener {
+            val intent = Intent(context, SignUpActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            }
+            startActivity(intent)
         }
     }
 
