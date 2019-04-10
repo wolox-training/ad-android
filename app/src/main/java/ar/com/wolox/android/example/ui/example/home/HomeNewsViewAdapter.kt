e91043b68f2ba0885cbab370fbadb4a437d7e17c
Alan Donoso Naumczuk <alan.donoso@wolox.com.ar>
2019-04-10 11:27:06 -0300

    Adapter used classes  modularized to different files

--- app/src/main/java/ar/com/wolox/android/example/ui/example/home/HomeNewsViewAdapter.kt
+++ app/src/main/java/ar/com/wolox/android/example/ui/example/home/HomeNewsViewAdapter.kt
@@ -1,18 +1,13 @@
 package ar.com.wolox.android.example.ui.example.home
 
 import android.support.v7.recyclerview.extensions.ListAdapter
-import android.support.v7.util.DiffUtil
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import ar.com.wolox.android.R
 import ar.com.wolox.android.example.model.News
 
 class HomeNewsViewAdapter(private val dataSet: List<News>) :
-        ListAdapter<News, HomeNewsViewAdapter.NewsItemViewHolder>(HomeNewsViewAdapter
-                .NewsDiffCallback()) {
+        ListAdapter<News, NewsItemViewHolder>(NewsDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsItemViewHolder {
         val itemView = LayoutInflater.from(parent.context)
@@ -28,17 +23,4 @@ class HomeNewsViewAdapter(private val dataSet: List<News>) :
     }
 
     override fun getItemCount(): Int = dataSet.size
-
-    class NewsItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-        val title: TextView = itemView.findViewById(R.id.vHomeNewsItemTitle)
-        val description: TextView = itemView.findViewById(R.id.vHomeNewsItemDescription)
-    }
-
-    class NewsDiffCallback : DiffUtil.ItemCallback<News>() {
-
-        override fun areContentsTheSame(n0: News, n1: News): Boolean = n0 == n1
-
-        override fun areItemsTheSame(n0: News, n1: News): Boolean = n0 == n1
-    }
 }
